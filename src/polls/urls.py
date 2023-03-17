4b9808f700e0a2d5bee42e213af96e62af6bfe89
graceyao99 <yaograceb@gmail.com>
2023-03-17 07:50:30 +0000

    my template

--- src/polls/urls.py
+++ src/polls/urls.py
@@ -1,6 +1,17 @@
+from unittest import result
 from django.urls import path
 from . import views
-
+app_name = 'polls'
 urlpatterns = [
+    #ex:/polls/
     path('', views.index, name = 'index'),
+
+    #ex:/polls/detail/5
+    path('detail/<int:question_id>/',views.detail, name='detail'),
+
+    #ex:/pools/results/5
+    path('results/<int:question_id>/',views.results, name='results'),
+
+    #ex:/polls/vote/5
+    path('vote/<int:question_id>/',views.vote, name='vote'),
 ]
--- src/polls/views.py
+++ src/polls/views.py
@@ -1,5 +1,20 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse,Http404
+from .models import Question
 
 def index(request):
-    return HttpResponse("Hello, World")
+    latest_question_list = Question.objects.order_by('pub_date')[:5] #affiche les 5 dernières questions
+    context = {'latest_question_list':latest_question_list}
+    return render(request,'polls/index.html',context)
+
+def detail(request,question_id):
+    question = get_object_or_404(Question,pk =question_id)
+    context = {'question':question}
+
+    return render(request,'polls/detail.html',context)
+
+def results(request,question_id):
+    return HttpResponse("tu regardes les résultats de la question %s." %question_id)
+
+def vote(request,question_id):
+    return HttpResponse("tu votes pour la question %s." %question_id)
